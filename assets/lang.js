9f8ceea231387c6575b1850b645640119454e715
Nicolas Joyard <joyard.nicolas@gmail.com>
2014-08-04 08:28:23 +0200

    Add multiple language support

--- assets/lang.js
+++ assets/lang.js
@@ -1,13 +1,39 @@
 /*jshint browser:true*/
 'use strict';
 
-var LANG = {
-  home: 'Accueil',
-  search: 'Rechercher',
-  searchTitle: 'Recherche: "%s"',
-  imageCount: '%s images',
-  galCount: '%s galeries',
-  breadcrumbSeparator: '⟩',
-  slideshowNext: '⟩',
-  slideshowPrev: '⟨'
-};
+var LANG = (function() {
+  var i18n = {
+    root: {
+      home: 'Home',
+      search: 'Search',
+      searchTitle: 'Search results: "%s"',
+      imageCount: '%s pictures',
+      galCount: '%s galleries',
+      breadcrumbSeparator: '⟩',
+      slideshowNext: '⟩',
+      slideshowPrev: '⟨'
+    },
+
+    fr: {
+      home: 'Accueil',
+      search: 'Rechercher',
+      searchTitle: 'Recherche: "%s"',
+      imageCount: '%s images',
+      galCount: '%s galeries'
+    }
+  };
+
+  var lang = window.navigator.language;
+  var candidates = [lang, lang.split('-')[0]];
+  var localized = i18n.root;
+
+  candidates.forEach(function(lang) {
+    if (lang in i18n) {
+      Object.keys(i18n[lang]).forEach(function(key) {
+        localized[key] = i18n[lang][key];
+      });
+    }
+  });
+
+  return localized;
+}());
